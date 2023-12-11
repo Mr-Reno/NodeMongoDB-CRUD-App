67ada69a9b1dece39510f200e92812bfea9f99b5
Mr-Reno <techdevlorenz@gmail.com>
2023-12-10 23:25:59 -0800

    2nd commit

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-import { Express } from "express";
-import mongoose from "mongoose";
-const app = Express()
+import express  from "express";
+import mongoose, {mongo} from "mongoose";
+const app = express();
+const port= process.env.PORT;
 
+const connectDB = async()=>{
+    try {
+        const conn = await mongoose.connect('mongodb+srv://renodb:sloan123@cluster0.md2deav.mongodb.net/renodb?retryWrites=true&w=majority');
+        console.log(`MongoDB is connected to ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Erro: ${error.message}`);
+        process.exit(1);
+    }
+}
 
-console.log();
+app.listen(port, ()=> console.log(`Listening to http://${port}`));
+connectDB();
